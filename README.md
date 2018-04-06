5eef0373645e3e2341e22ec568943bc97e4f2b28
thetuningspoon <mike@thetuningspoon.com>
2018-04-06 11:43:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# SI Multirotator - Takes a set of image thumbnails or other navigation elements and cycles through a corresponding set of images/content
+Developed by Mike Spooner (thetuningspoon) for Solution Innovators
+
+SI Modal enables opening any page or submitting any form to a responsive modal window.
+
+## Usage
+
+1. Load si-multirotator.js in the head section of your page (after jQuery)
+2. Initialize the function somewhere in your javascript:
+
+```
+$(function() {
+    new SiMultiRotator({
+        transitionSpeed: 200,
+        autoTransitionSpeed: 1000,
+        slideTimeout: 5000,
+        thumbnailSelector: '.thumb',
+        imageSelector: '.img',
+        captionSelector: '.caption',
+        pauseOverSelector: '.thumb, .img, .caption',
+    });
+}
+```
+3. Each element on the page (thumbnail, image, and caption) must have a data-index attribute with the numerical index of the element (starting at 0). For example, the thumbnail for a particular image, should have a data-index that matches a data-index on the corresponding full size image it represents. The appropriate caption element should also share the same index:
+
+```
+<img class="thumb" src="" data-index="0" />
+<img class="thumb" src="" data-index="1" />
+<img class="thumb" src="" data-index="2" />
+
+<img class="img" src="" data-index="0" />
+<img class="img" src="" data-index="1" />
+<img class="img" src="" data-index="2" />
+
+<div class="caption" data-index="0" />
+<div class="caption" data-index="1" />
+<div class="caption" data-index="2" />
+```
+
+You may initialize the module more than once on the same page by using different selectors for the thumbnails, images, and captions.
---
+++ si-multirotator.js
@@ -0,0 +1,110 @@
+/**
+ * SI MultiRotator 0.0.1
+ * Developed by Mike Spooner (thetuningspoon) for Solution Innovators 2018
+ *
+ * BASIC USAGE
+ *
+ * $(function() {
+ *      new SiMultiRotator({
+ *          transitionSpeed: 200,
+ *          autoTransitionSpeed: 1000,
+ *          slideTimeout: 5000,
+ *          thumbnailSelector: '.thumb',
+ *          imageSelector: '.img',
+ *          captionSelector: '.caption',
+ *          pauseOverSelector: '.thumb, .img, .caption',
+ *      });
+ * }
+ *
+ * Each element on the page (thumbnail, image, and caption) must have a data-index attribute with the numerical index of the element (starting at 0). For example, the thumbnail for a particular image, should have a data-index that matches a data-index on the corresponding full size image it represents. The appropriate caption element should also share the same index:
+ *
+ * <img class="thumb" src="" data-index="0" />
+ * <img class="thumb" src="" data-index="1" />
+ * <img class="thumb" src="" data-index="2" />
+ *
+ * <img class="img" src="" data-index="0" />
+ * <img class="img" src="" data-index="1" />
+ * <img class="img" src="" data-index="2" />
+ *
+ * <div class="caption" data-index="0" />
+ * <div class="caption" data-index="1" />
+ * <div class="caption" data-index="2" />
+ *
+ */
+
+function SiMultiRotator(options) {
+
+    // Default configuration options if none specified
+    var defaults = {
+        transitionSpeed: 200,
+        autoTransitionSpeed: 1000,
+        slideTimeout: 5000,
+        thumbnailSelector: '.thumb',
+        imageSelector: '.img',
+        captionSelector: '.caption',
+        pauseOverSelector: '.thumb, .img, .caption',
+        startingIndex: 0,
+    };
+    var config = $.extend({}, defaults, options); // Merge the defaults and user specified options into config
+
+    // Properties
+    var $thumbnails = $(config.thumbnailSelector);
+    var $displayImgs = $(config.imageSelector);
+    var $captions = $(config.captionSelector);
+    var $pauseOverElements = $(config.pauseOverSelector);
+    var currentIndex = null;
+    var slideshowInterval;
+
+    // Initialization
+    $thumbnails.on('click', function () {
+        $this = $(this);
+        switchSlide(parseInt($this.attr('data-index')));
+    });
+
+    $pauseOverElements.on('mouseenter', function () {
+        //console.log('entered');
+        stopSlideshow();
+    });
+    $pauseOverElements.on('mouseleave', function () {
+        //console.log('left');
+        startSlideshow();
+    });
+
+    // Wait until the starting slide image has fully loaded before showing it
+    $displayImgs.first().on('load', function () {
+        switchSlide(config.startingIndex);
+        startSlideshow();
+    });
+
+
+    function startSlideshow() {
+        slideshowInterval = setInterval(function () {
+            if ((currentIndex + 1) < $thumbnails.length) {
+                switchSlide(currentIndex + 1, config.autoTransitionSpeed);
+            }
+            else {
+                switchSlide(0, config.autoTransitionSpeed);
+            }
+        }, config.slideTimeout);
+    }
+
+    function stopSlideshow() {
+        clearInterval(slideshowInterval);
+    }
+
+    function switchSlide(index, speed) {
+        if(!speed) speed = config.transitionSpeed;
+
+        if(index !== currentIndex) {
+            $thumbnails.removeClass('active');
+            $thumbnails.filter('[data-index="' + index + '"]').addClass('active');
+
+            currentIndex = index;
+            $displayImgs.fadeOut(speed);
+            $displayImgs.filter('[data-index="' + index + '"]').fadeIn(speed);
+
+            $captions.fadeOut(speed);
+            $captions.filter('[data-index="' + index + '"]').fadeIn(speed);
+        }
+    }
+}
